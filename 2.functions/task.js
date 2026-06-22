dc83ea4c34ed2be72133af9e203bdade0fe6c004
Neto Andrey <diomand761988@gmail.com>
2026-06-22 21:05:02 +0300

    Реализована первая задача

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,31 @@
 function getArrayParams(...arr) {
+    if (arr.length === 0) {
+        return {
+            min: 0,
+            max: 0,
+            avg: 0
+        };
+    }
 
-  return { min: min, max: max, avg: avg };
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if (num > max) max = num;
+        if (num < min) min = num;
+        sum += num;
+    }
+
+    const avg = sum / arr.length;
+    const roundedAvg = Number(avg.toFixed(2));
+
+    return {
+        min,
+        max,
+        avg: roundedAvg
+    };
 }
 
 function summElementsWorker(...arr) {
