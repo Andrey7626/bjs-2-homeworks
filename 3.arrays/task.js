8adcb99ed9286b4d8e176682784e34907f4f1bb0
Neto Andrey <diomand761988@gmail.com>
2026-06-29 18:31:06 +0300

    Реализована вторая задача

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -7,5 +7,12 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    const averageAge = totalAge / filteredUsers.length;
 }
